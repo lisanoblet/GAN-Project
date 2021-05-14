73ef8e49ae4677634f687a5d6ffb345bb3eb4d96
lisanoblet <60179620+lisanoblet@users.noreply.github.com>
2021-05-14 19:42:05 +0200

    Ajout des liens des autres modèles

--- p5Template-master/src/sketch.ts
+++ p5Template-master/src/sketch.ts
@@ -32,8 +32,29 @@ function draw() {
 function setup() {
     p6_CreateCanvas()
     const model = new rw.HostedModel({
+        // Style GAN (nébuleuses)
         url: "https://stylegan-9d4f8d9c.hosted-models.runwayml.cloud/v1/",
         token: "8qc1au2OaebjTFSjwL6QLg==",
+        
+        // ReeffishGAN
+        /*url: "https://reeffishgan-958f2802.hosted-models.runwayml.cloud/v1/",
+        token: "6IaywDT1bxDYv9hOOzXiyA==",*/
+        
+        // CoralGAN
+        /*url: "https://coralgan-e0d82ae2.hosted-models.runwayml.cloud/v1/",
+        token: "SX7PZNGf1RquuGiYtqsUCg==",*/
+        
+        //Earth_Image_Simulator
+        /*url: "https://earth-image-generator-4911471b.hosted-models.runwayml.cloud/v1/",
+        token: "kH28KZpjsu25SlZQAK466g==",*/
+        
+        // Seascapes
+        /*url: "https://seascapes-217d37c7.hosted-models.runwayml.cloud/v1/",
+        token: "AR6xRpcZLrjyxjPA7bxGsg==",*/
+        
+        //BigGAN
+        /*url: "https://biggan-296ffb3e.hosted-models.runwayml.cloud/v1/",
+        token: "+p4wo2Qdk9YlHw+Po03Tfw==",*/
       });
 
     for (let i = 0; i<50; ++i){
@@ -46,7 +67,7 @@ function setup() {
         "truncation": 0.8,
       };
       model.query(inputs).then(outputs => {
-        const { image } = outputs;
+        const { image } = outputs; //remplacer image par generated_output pour biggan
         img = createImg(image)
       });
   }
